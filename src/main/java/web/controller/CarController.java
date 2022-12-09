a3cfc9f48255feb7a86e9a0d3dcfef5e9f402657
Daria <vda117169@gmail.com>
2022-12-09 09:35:20 +0500

    правки контроллера

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import web.model.Car;
-import web.service.CarServiseImpl;
+import web.service.CarService;
 
 import java.util.List;
 
@@ -14,16 +14,16 @@ import java.util.List;
 @RequestMapping("/cars")
 public class CarController {
 
-    public final CarServiseImpl carServiseImp;
+    public final CarService carService;
 
-    public CarController(CarServiseImpl carServiseImp) {
-        this.carServiseImp = carServiseImp;
+    public CarController(CarService carService) {
+        this.carService = carService;
 
     }
 
     @GetMapping("/cars")
     public String getCars(@RequestParam(value = "count", required = false) Integer count, Model model) {
-        List<Car> cars = carServiseImp.countsCarsIdService(count);
+        List<Car> cars = carService.countsCarsIdService(count);
         model.addAttribute("cars",cars);
         return "cars";
     }
